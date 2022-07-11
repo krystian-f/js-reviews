c7b8d55a8548afcc3ce454e99fd6ab918d7853f3
krystian-f <krystianfpa@gmail.com>
2022-07-11 09:52:46 +0200

    JS functions: LoadReviews and Display next/previous review

--- app.js
+++ app.js
@@ -11,30 +11,83 @@ const reviewsDB = [
   },
   {
     id: 2,
-    user: "John Smith",
-    avatar: "./img/avatar-man-1.png",
-    score: 4.5,
+    user: "Kyle Johnson",
+    avatar: "./img/avatar-woman-1.png",
+    score: 5,
     review: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique temporibus doloribus veniam quidem ipsa laboriosam, iure perspiciatis aspernatur minima optio!"
   },
   {
     id: 3,
-    user: "John Smith",
-    avatar: "./img/avatar-man-1.png",
-    score: 4.5,
+    user: "Mark Twain",
+    avatar: "./img/avatar-man-2.png",
+    score: 4,
     review: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae iste in veritatis. Iusto, dolorem rem."
   }
 ];
 
 // DOM
 const btns = document.querySelectorAll('.js-reviews__button');
+const review = document.querySelector('.js-reviews__review');
 
+// Variables
+let reviewId = Math.floor(Math.random() * 3);
+
+
+// Functions
+let cleanReviewContent = function(){
+  review.innerHTML='';
+}
+
+let loadScore = function(id){
+  console.log(reviewsDB[id].score);
+}
+  loadScore(reviewId);
+
+let loadReview = function(id){
+  let reviewData = 
+  `
+    <div class="reviews__img"><img src="${reviewsDB[id].avatar}" class="reviews__img--resize" alt="review-img"></div>
+    <div class="reviews__author">${reviewsDB[id].user}</div>
+    <div class="reviews__score">
+    ${reviewsDB[id].score}
+    </div>
+    <div class="reviews-_description">${reviewsDB[id].review}</div>
+  `;
+  review.insertAdjacentHTML("afterbegin", reviewData);
+  console.log(id);
+}
+
+let displayPreviousReview = function(){
+  if(reviewId>0){
+    reviewId-=1;
+    loadReview(reviewId);
+  }else if(reviewId<=0){
+    reviewId = reviewsDB.length -1;
+    loadReview(reviewId);
+  }
+}
+
+let displayNextReview = function(){
+  if(reviewId<reviewsDB.length -1){
+    reviewId+=1;
+    loadReview(reviewId);
+  }else if(reviewId>=reviewsDB.length -1){
+    reviewId=0;
+    loadReview(reviewId);
+  }
+
+}
+
+loadReview(reviewId);
 
 btns.forEach(btn => {
-  btn.addEventListener('click', ()=>{
+  btn.addEventListener('click', (e)=>{
+    cleanReviewContent();
+
     if (btn.classList.contains('js-reviews__button--prev')) {
-      console.log('Previous')
+      displayPreviousReview();
     } else if (btn.classList.contains('js-reviews__button--next')){
-      console.log('Next');
+      displayNextReview();
     }
   })
 })
